1f156745802502e6056145c1c9d1bde7c9b06adc
Atharv Dange <atharvdange618@gmail.com>
2024-08-27 12:32:10 +0530

    removed auth middleware from appointment api

--- backend/routes/appointment.routes.js
+++ backend/routes/appointment.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 import { addAppointment, getAppointments, deleteAppointment, } from '../controllers/appointment.controllers.js';
 import { protectRoute } from '../middleware/protectRoute.js';
 
-router.post('/appointments', protectRoute, addAppointment);        // Add a new appointment
+router.post('/appointments', addAppointment);        // Add a new appointment
 router.get('/patients', protectRoute, getAppointments);            // Get all appointments
 router.delete('/appointments/:id', protectRoute, deleteAppointment); // Delete an appointment
 
